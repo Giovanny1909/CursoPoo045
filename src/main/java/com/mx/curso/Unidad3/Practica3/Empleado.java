460e0cfd923648292a5dae436f1ca65a50e8a98f
Giovanny <mendez.giovanny2001@gmail.com>
2025-12-04 23:16:02 -0600

    Práctica 3: Polimorfismo en el Restaurante (Gestión de Personal)

---
+++ src/main/java/com/mx/curso/Unidad3/Practica3/Empleado.java
@@ -0,0 +1,42 @@
+package com.mx.curso.Unidad3.Practica3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Empleado {
+    //metodo concreto trabajar
+    public void trabajar() {
+        System.out.println("El empleado está trabajando.");
+    }
+    //metodo abstracto saludar
+    public void saludar() {
+        System.out.println("Hola, soy un empleado.");
+    }
+}
+class Cocinero extends Empleado {
+    @Override
+    public void saludar() {
+        System.out.println("Hola, soy un cocinero.");
+    }
+}
+class Mesero extends Empleado {
+    @Override
+    public void saludar() {
+        System.out.println("Hola, soy un mesero.");
+    }
+}
+class SistemaRestaurante {
+    public static void main (String[] args) {
+        List<Empleado> listaEmpleados = new ArrayList<>();
+        listaEmpleados.add(new Cocinero());
+        //instancias de las sub clases
+        listaEmpleados.add(new Cocinero());
+        listaEmpleados.add(new Mesero());
+        listaEmpleados.add(new Mesero());
+
+        for (Empleado e : listaEmpleados) {
+            e.saludar();
+            e.trabajar();
+        }
+    }
+}
