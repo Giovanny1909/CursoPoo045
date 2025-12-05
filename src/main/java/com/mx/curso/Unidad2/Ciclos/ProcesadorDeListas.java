cef525dc99718e291e1ad324ae173a3f7bd15c3d
Giovanny <mendez.giovanny2001@gmail.com>
2025-12-04 21:05:32 -0600

    Toda la unidad 2

--- src/main/java/com/mx/curso/Unidad2/Ciclos/ProcesadorDeListas.java
+++ src/main/java/com/mx/curso/Unidad2/Ciclos/ProcesadorDeListas.java
@@ -19,4 +19,23 @@ public class ProcesadorDeListas {
             indice++;
         }
     }
+
+    public static class RecetaDeCocina {
+        static void main(String[] args) {
+            int ingredientesPreparados=0;
+            int tipoDeCocina =0;
+
+            System.out.println("Paso 1: Preparar los ingredientes");
+            System.out.println(ingredientesPreparados++);
+            System.out.println("Agregar ingrediente al platillo");
+            System.out.println(ingredientesPreparados++);
+
+            System.out.println("Paso 2: Cocina Platillo");
+            System.out.println(tipoDeCocina++);
+
+            System.out.println("Paso 3: Servir el platillo");
+
+        }
+
+    }
 }
